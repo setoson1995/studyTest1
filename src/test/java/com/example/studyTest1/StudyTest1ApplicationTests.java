ed6eabc60093428f7bb20930a342e770070f0cbc
손성우 <setoson1995@3top.co.kr>
2023-03-14 12:43:12 +0900

    Initial commit

---
+++ src/test/java/com/example/studyTest1/StudyTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
